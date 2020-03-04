72fcaa000001b7a38778ca1c1ba4dff77e842f0b
founasrahou <founasrahou@hotmail.com>
2020-03-03 18:27:26 -0800

    add Login

--- src/test/java/com/cybertek/tests/day2_locators/FindElement.java
+++ src/test/java/com/cybertek/tests/day2_locators/FindElement.java
@@ -1,7 +1,39 @@
 package com.cybertek.tests.day2_locators;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FindElement {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+// locator 1:ID
+        //// GO TO CYBERTEK OKTA LOGIN PAGE
+        // write to the text box
+        //
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get(" https://cybertekschool.okta.com/");
+        // write email to the text
+        // find element method from webdriver
+
+        WebElement emailBox = driver.findElement(By.id("okta-signin-username"));
+
+        emailBox.sendKeys("founasrahou@hotmail.com");
+        Thread.sleep(3000);
+        WebElement password = driver.findElement(By.id("okta-signin-password"));
+
+        password.sendKeys("@@Bakery9220");
+        Thread.sleep(3000);
+        WebElement signIn = driver.findElement(By.className("button button-primary"));
+        signIn.click();
+
+        Thread.sleep(3000);
+
+//        Thread.sleep(3000);
+//        WebElement pushBotton = driver.findElement(By.id("button button-primary"));
+//        pushBotton.click();
 
     }
 }
---
+++ src/test/java/com/cybertek/tests/day2_locators/Login.java
@@ -0,0 +1,45 @@
+package com.cybertek.tests.day2_locators;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+    public static void main(String[] args) {
+        // user story : as i user i should be able to log in vytrack
+        //***test case1*****
+        // 1-go to Vytrack login page
+        //2- write username data:storemanager52
+        // write password
+        //click login button
+        // verify that the user is in the home page
+        // ====>test case 2 login invalid cridential====>>>>
+        // go to log in page
+        // write invalid name
+        // write invalid password
+        //
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get(" https://app.vytrack.com/user/login");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("prependedInput")).sendKeys("Storemanager51");
+        driver.findElement(By.id("prependedInput2")).sendKeys("UserUser123");
+        //// FIND LOGIN BUTTON AND CLICK
+        WebElement loginButton= driver.findElement(By.id("_submit"));
+        loginButton.click();
+        // verify that you are in the home page
+        String expectedTitle = "Dashboard";
+        String actualTitle = driver.getTitle();
+        if(expectedTitle.equalsIgnoreCase(actualTitle)){
+            System.out.println("actualTitle = " + actualTitle);
+        }
+      
+
+
+
+
+
+    }
+}
